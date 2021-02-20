666b96fe6ab95c8a335ea555d8c6f6acf270f274
optimistic <optimistic@optimisticnoMacBook-Pro.local>
2021-02-20 23:20:36 +0900

    Node.js task1 first commit

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+const express = require("express");
+const app = express();
+
+// テンプレートエンジンの指定
+app.set("view engine", "ejs");
+app.use(express.static('public'))
+
+app.get("/", function (req, res) {
+  const data = {
+    title: "Login",
+    forms: [
+      { name: "E-Mail Adress" },
+      { name: "Password" },
+    ],
+    message:"Forgot Your Password"
+  };
+  // レンダリングを行う
+  res.render("./login.ejs", data);
+});
+
+app.get("/register", function (req, res) {
+  const data = {
+    title: "Register",
+    forms: [
+      { name: "Name" },
+      { name: "E-Mail Adress" },
+      { name: "Password" },
+      { name: "Confirm Password" },
+    ],
+    message: ""
+  };
+  // レンダリングを行う
+  res.render("./login.ejs", data);
+})
+
+app.listen(3000);
 
