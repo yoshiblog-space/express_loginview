46876d8fac84b968029ec576f8949d2ebb0a470c
optimistic <optimistic@optimisticnoMacBook-Pro.local>
2021-02-25 17:53:35 +0900

    delete node_moudles

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,31 +1,61 @@
-const express = require('express');
 const Views = '../views/'
+const { check, validationResult } = require('express-validator');
+
+const loginView = {
+  title: 'Login',
+  forms: [
+    { name: 'E-Mail Adress' },
+    { name: 'Password' },
+  ],
+  errormessages: '',
+  message: 'Forgot Your Password'
+};
+const registerView = {
+  title: 'Register',
+  forms: [
+    { name: 'Name' },
+    { name: 'EmailAdress' },
+    { name: 'Password' },
+    { name: 'ConfirmPassword' },
+  ],
+  errormessages: '',
+  message: ''
+};
+const dashboardView = {
+  title: 'DashBoard',
+  username: '',
+  message: 'You are Log in!'
+};
 
 module.exports = {
   doGetLogin(req, res) {
-    const data = {
-      title: 'Login',
-      forms: [
-        { name: 'E-Mail Adress' },
-        { name: 'Password' },
-      ],
-      message: 'Forgot Your Password'
-    };
-    // レンダリングを行う
-    res.render(Views + 'login.ejs', data);
+    res.render(Views + 'login.ejs', loginView);
   },
   doGetRegister(req, res) {
-    const data = {
-      title: 'Register',
-      forms: [
-        { name: 'Name' },
-        { name: 'E-Mail Adress' },
-        { name: 'Password' },
-        { name: 'Confirm Password' },
-      ],
-      message: ''
-    };
-    // レンダリングを行う
-    res.render(Views + 'login.ejs', data);
+    registerView.errormessages = '';
+    res.render(Views + 'login.ejs', registerView);
+  },
+  doGetDashBoard(req, res) {
+    res.render(Views + 'dashboard.ejs', dashboardView);
+  },
+  validationList: [
+    check('Name')
+      .notEmpty().withMessage('名前を入力してください。'),
+    check('EmailAdress')
+      .isEmail().withMessage('メールアドレスを入力して下さい'),
+    check('Password')
+      .notEmpty().withMessage('パスワードを入力してください。')
+      .isLength({ min: 7 }).withMessage('パスワードを7文字以上で入力してください。'),
+    check('ConfirmPassword')
+      .custom((value, { req }) => value === req.body.Password)
+      .withMessage('パスワードが一致しません'),
+  ],
+  doGetRegisterErr(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      registerView.errormessages = errors.array();
+      return res.render(Views + 'login.ejs', registerView);
+    }
+    res.redirect('/dashboard');
   }
 }
