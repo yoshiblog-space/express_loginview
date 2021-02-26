82624c8e4309c8c3a59b0f8da07b134680fbfe36
optimistic <optimistic@optimisticnoMacBook-Pro.local>
2021-02-26 12:38:46 +0900

    divide validatefile

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const { check, validationResult } = require('express-validator');
 const userController = require('./controllers/UserController')
-
+var testValidator = require('./validator/TestValidator.js')
 const Views = '../views/'
 
 // テンプレートエンジンの指定
@@ -12,6 +13,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.get('/', userController.doGetLogin);
 app.get('/register', userController.doGetRegister);
-app.post('/register', userController.validationList, userController.doGetRegisterErr);
+app.post('/register', testValidator, userController.doGetRegisterErr);
 app.get('/dashboard', userController.doGetDashBoard);
 app.listen(3000);
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,5 @@
 const Views = '../views/'
-const { check, validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const loginView = {
   title: 'Login',
@@ -38,18 +38,6 @@ module.exports = {
   doGetDashBoard(req, res) {
     res.render(Views + 'dashboard.ejs', dashboardView);
   },
-  validationList: [
-    check('Name')
-      .notEmpty().withMessage('名前を入力してください。'),
-    check('EmailAdress')
-      .isEmail().withMessage('メールアドレスを入力して下さい'),
-    check('Password')
-      .notEmpty().withMessage('パスワードを入力してください。')
-      .isLength({ min: 7 }).withMessage('パスワードを7文字以上で入力してください。'),
-    check('ConfirmPassword')
-      .custom((value, { req }) => value === req.body.Password)
-      .withMessage('パスワードが一致しません'),
-  ],
   doGetRegisterErr(req, res) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
